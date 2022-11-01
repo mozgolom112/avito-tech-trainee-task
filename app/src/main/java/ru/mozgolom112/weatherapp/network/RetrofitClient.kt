925e1e37fe8c76e2b1e6525f5d147e669b2839ae
Nikita <cell-26@yandex.ru>
2022-11-01 23:26:19 +0300

    Незначительные изменения

--- app/src/main/java/ru/mozgolom112/weatherapp/network/RetrofitClient.kt
+++ app/src/main/java/ru/mozgolom112/weatherapp/network/RetrofitClient.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import ru.mozgolom112.weatherapp.utils.BASE_URL
+import ru.mozgolom112.weatherapp.utils.consts.BASE_URL
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
